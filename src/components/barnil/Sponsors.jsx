cc1da2dea6fd41ee57405d6e81d4e57ff40880c1
Barnil Sarma <sarmabarnil494@gmail.com>
2023-08-24 05:37:27 +0000

    fourth commit

--- src/components/barnil/Sponsors.jsx
+++ src/components/barnil/Sponsors.jsx
@@ -1,30 +1,32 @@
-import './Nav_footer_events_sponsors_speakers.css';
-import Surround from './surround.svg';
+import "./Nav_footer_events_sponsors_speakers.css";
+import Surround from "./surround.svg";
+import sbi from "./sponsors-img/sbi.svg";
+import mutual from "./sponsors-img/mutual-fund.svg";
+import indianOil from "./sponsors-img/indian-oil.svg";
+import assamTribune from "./sponsors-img/assam-tribune.svg";
 function Sponsors() {
-    const sponsors_info = [
-      { logo: "logo1" },
-      { logo: "logo2" },
-      { logo: "logo3" },
-      { logo: "logo4" },
-      { logo: "logo5" },
-      { logo: "logo6" },
-      { logo: "logo7" },
-    ];
-    return (
-      <div className="sponsors">
-        <center>
-          <div className="surrounder">
-            <img src={Surround} />
-            <h1>SPONSORS</h1>
-            <img src={Surround} />
-          </div>
-        </center>
-        <div className="sponsors-img">
-          {sponsors_info.map((sponsor) => (
-            <img src={sponsor.logo} />
-          ))}
+  const sponsors_info = [
+    { logo: "./sponsors-img/sbi.svg" },
+    { logo: "./sponsors-img/mutual-fund.svg" },
+    { logo: "./sponsors-img/indian-oil.svg" },
+    { logo: "./sponsors-img/assam-tribune.svg" },
+  ];
+  return (
+    <div className="sponsors">
+      <center>
+        <div className="surrounder">
+          <img src={Surround} />
+          <h1>SPONSORS</h1>
+          <img src={Surround} />
         </div>
+      </center>
+      <div className="sponsors-img">
+        <img src={sbi} />
+        <img src={mutual} />
+        <img src={indianOil} />
+        <img src={assamTribune} />
       </div>
-    );
-  }
-  export default Sponsors;
+    </div>
+  );
+}
+export default Sponsors;
